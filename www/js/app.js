f10b93fcd432842cc4bc374a3c908d51bae0e630
Fernando Contreras <fercreek@gmail.com>
2015-10-16 06:04:23 -0500

    refresh stories

--- www/js/app.js
+++ www/js/app.js
@@ -2,21 +2,38 @@
 var app = angular.module('myreddit', ['ionic', 'angularMoment']);
 
 app.controller('RedditCtrl', function($scope, $http){
-    $scope.stories = [
+    $scope.stories = [];
 
-    ];
+    function loadStories(params, callback){
+        $http.get('https://www.reddit.com/r/Android/new/.json', {params:params})
+            .success(function(response){
+                var stories = [];
+                angular.forEach(response.data.children, function(child){
+                    stories.push(child.data);
+                });
+                // $scope.$broadcast('scroll.infiniteScrollComplete');
+                callback(stories);
+            });
+    }
+
     $scope.loadOlderStories = function(){
         var params = {};
         if ($scope.stories.length > 0){
-            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+            params.after = $scope.stories[$scope.stories.length - 1].name;
         }
-        $http.get('https://www.reddit.com/r/Android/new/.json', {params:params})
-            .success(function(response){
-                angular.forEach(response.data.children, function(child){
-                    $scope.stories.push(child.data);
-                });
-                $scope.$broadcast('scroll.infiniteScrollComplete');
-            });
+        loadStories(params, function(olderStories){
+            $scope.stories = $scope.stories.concat(olderStories);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
+    $scope.loadNewerStories = function(){
+        var params = {"before": $scope.stories[0].name};
+        loadStories(params, function(newerStories){
+            $scope.stories = newerStories.concat($scope.stories);
+            $scope.$broadcast('scroll.refreshComplete');
+
+        });
     };
 });
 
